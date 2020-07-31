e5ff5cdd000041ef25d55f3b20d2defecdc657e0
Peter Sagan <petersagan@Peters-MacBook-Pro.local>
2020-07-31 08:02:44 +0200

    remove seaborn

--- 020-strava-ride-power-graph/04_graph.py
+++ 020-strava-ride-power-graph/04_graph.py
@@ -3,18 +3,13 @@ import pandas as pd
 # read and filter previously serialized data
 df = pd.read_pickle("./efforts.pkl")
 df = df[(df.avg_grade > -3) & (df.avg_grade < 24)]
-print(df)
+print(df.head())
 
 import matplotlib.pyplot as plt
 
 # draw bar chart of power per elevation grade
-plt.bar(df['avg_grade'],df['avg_power'], 0.25)
+plt.bar(df['avg_grade'],df['avg_power'], 0.2, alpha=0.5)
 plt.xlabel('Grade (%)')
 plt.ylabel('Power (W)')
 plt.title('Activity efforts')
-plt.show()
-
-import seaborn as sns
-
-sns.regplot(x='avg_grade',y='avg_power',data=df[['avg_grade','avg_power']], fit_reg=True)
 plt.show()
